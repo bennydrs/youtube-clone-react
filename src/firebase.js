371323622165646cc25cb972bdb41e573ddf9be0
bennydrspc <bennydrm16@gmail.com>
2020-11-09 10:44:51 +0700

    header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB9kVOOGD_ftPXLq0K2M3GIZ3rGawNxGRk",
+  authDomain: "clone-bc018.firebaseapp.com",
+  databaseURL: "https://clone-bc018.firebaseio.com",
+  projectId: "clone-bc018",
+  storageBucket: "clone-bc018.appspot.com",
+  messagingSenderId: "513214387696",
+  appId: "1:513214387696:web:971d528505f31150a73935",
+  measurementId: "G-S1YNMP0EMN"
+};
